9641346fe83db9c80333e2363df7a75c2daf7d97
Marek Šípek <123970857+Sipisak@users.noreply.github.com>
2026-07-30 10:46:11 +0200

    feat: provide language and theme preferences

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Portfolio from "./App";
+import { PreferencesProvider } from "./preferences";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Portfolio />
+    <PreferencesProvider>
+      <Portfolio />
+    </PreferencesProvider>
   </StrictMode>
 );
